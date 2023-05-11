e7d23d2ca5f0c8c06864e8967e1d535efc2ea76d
Natal Mirosh <Papina888@yandex.ru>
2023-05-11 14:16:50 +0600

    exercise 3 create

---
+++ M_03_DatabaseAndModels/mysite/shopapp/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
+
+
+def shop_index(request: HttpRequest):
+    products = [
+        ('Orange', 55),
+        ('Bananas', 130),
+        ('Ananas', 100),
+    ]
+
+    context = {
+        'products': products
+    }
+
+
+
+    return render(request, 'shopapp/shop-index.html', context=context)
