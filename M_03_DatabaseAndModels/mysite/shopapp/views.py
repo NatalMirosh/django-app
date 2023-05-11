d36c5744b18e449fbbbe808af9f1c1e32dac24d6
Natal Mirosh <Papina888@yandex.ru>
2023-05-11 18:04:11 +0600

    exercise 3 done

--- M_03_DatabaseAndModels/mysite/shopapp/views.py
+++ M_03_DatabaseAndModels/mysite/shopapp/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
+from .models import Product, Order
 
 def shop_index(request: HttpRequest):
     products = [
@@ -16,3 +17,16 @@ def shop_index(request: HttpRequest):
 
 
     return render(request, 'shopapp/shop-index.html', context=context)
+
+def products_list(request: HttpRequest):
+    context = {
+        "products": Product.objects.all(),
+    }
+    return render(request, 'shopapp/products-list.html', context=context)
+
+
+def orders_list(request: HttpRequest):
+    context = {
+        "orders": Order.objects.select_related("user").prefetch_related("products").all(),
+    }
+    return render(request, 'shopapp/orders-list.html', context=context)
