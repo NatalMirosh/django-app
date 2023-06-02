140a2eab87e2dda2849ea50f64da6dc41c129c77
Natal Mirosh <Papina888@yandex.ru>
2023-06-02 21:12:41 +0600

    exercise 10

--- M_10_Testing/mysite/shopapp/tests.py
+++ M_10_Testing/mysite/shopapp/tests.py
@@ -1,3 +1,130 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.conf import settings
+from django.contrib.auth.models import User
+from shopapp.utils import add_two_numbers
+from string import ascii_letters
+from random import choices
 
-# Create your tests here.
+from shopapp.models import Product
+
+
+class AddTwoNumbersTestCase(TestCase):
+    def test_add_two_numbers(self):
+        result = add_two_numbers(2, 3)
+        self.assertEqual(result, 5)
+
+
+class ProductCreateViewTestCase(TestCase):
+    def setUp(self) -> None:
+        self.product_name = "".join(choices(ascii_letters, k=10))
+        Product.objects.filter(name=self.product_name).delete()
+
+    def test_create_product(self):
+        response = self.client.post(
+            reverse("shopapp:product_create"),
+            {
+                "name": self.product_name,
+                "price": "123.45",
+                "description": "A good table",
+                "discount": "10",
+            }
+        )
+        self.assertRedirects(response, reverse("shopapp:products_list"))
+        self.assertTrue(
+            Product.objects.filter(name=self.product_name).exists()
+        )
+
+
+class ProductDetailsViewTestCase(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.product = Product.objects.create(name="Best Product")
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.product.delete()
+
+    def test_get_product(self):
+        response = self.client.get(
+            reverse("shopapp:product_details", kwargs={"pk": self.product.pk})
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_product_and_check_content(self):
+        response = self.client.get(
+            reverse("shopapp:product_details", kwargs={"pk": self.product.pk})
+        )
+        self.assertContains(response, self.product.name)
+
+
+class ProductsListViewTestCase(TestCase):
+    fixtures = [
+        'products-fixture.json',
+    ]
+
+    def test_products(self):
+        response = self.client.get(reverse("shopapp:products_list"))
+        # products =  Product.objects.filter(archived=False).all()
+        # products_ = response.context["products"]
+        # for p, p_ in zip(products, products_):
+        #     self.assertEqual(p.pk, p_.pk)
+        self.assertQuerysetEqual(
+            qs=Product.objects.filter(archived=False).all(),
+            values=(p.pk for p in response.context["products"]),
+            transform=lambda p: p.pk,
+        )
+        self.assertTemplateUsed(response, 'shopapp/products-list.html')
+
+
+class OrdersListViewTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.credentials = dict(username='bob', password='qwerty')
+        cls.user = User.objects.create_user(**cls.credentials)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.user.delete()
+
+    def setUp(self) -> None:
+        self.client.login(**self.credentials)
+
+    def test_orders_view(self):
+        response = self.client.get(reverse("shopapp:orders_list"))
+        self.assertContains(response, "Orders")
+
+    def test_orders_view_not_authenticated(self):
+        self.client.logout()
+        response = self.client.get(reverse("shopapp:orders_list"))
+        #self.assertRedirects(response, str(settings.LOGIN_URL))
+        self.assertEqual(response.status_code, 302)
+        self.assertIn(str(settings.LOGIN_URL), response.url)
+
+
+class ProductsExportViewTestCase(TestCase):
+    fixtures = [
+        'products-fixture.json',
+    ]
+
+    def test_get_products(self):
+        response = self.client.get(
+            reverse("shopapp:products-export"),
+        )
+        self.assertEqual(response.status_code, 200)
+        products = Product.objects.order_by("pk").all()
+        expected_data = [
+            {
+                "pk": product.pk,
+                "name": product.name,
+                "price": str(product.price),
+                "archived": product.archived,
+            }
+            for product in products
+        ]
+        products_data = response.json()
+        self.assertEqual(
+            products_data["products"],
+            expected_data
+        )
